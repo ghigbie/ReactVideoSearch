a9e0860cd140c1f4b389e8c7c839482e8464ba58
George Higbie <gehigbie@gmail.com>
2018-03-06 15:15:26 +0000

    Change dir structure and import package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar'
+import APIKey from './services/apiKey';
+
+YTSearch({Key: APIKey, term: 'smurfs'}, (data) => {
+  console.log(data);
+});
 
 class App extends Component {
   render() {
