be1b4a357f04a1afe8110e8820715d9a252c87dd
George Higbie <gehigbie@gmail.com>
2018-03-06 16:12:46 +0000

    Add bootstrap stylesheet link

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import './App.css';
 
-import SearchBar from './components/SearchBar/SearchBar'
+import SearchBar from './components/SearchBar/SearchBar';
+import VideoList from './components/VideoList/VideoList';
 import APIKey from './services/apiKey';
-
-YTSearch({key: APIKey, term: 'smurfs'}, (data) => {
-  console.log(data);
-});
 
 class App extends Component {
   constructor(props){
     super(props)
     
     this.state = {videos: []};
+    
+    YTSearch({key: APIKey, term: 'smurfs'}, videos => this.setState({videos}));
   }
   
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
