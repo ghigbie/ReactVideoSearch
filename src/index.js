c270628996fd9c9e6ca2e228f352639ef0118413
George Higbie <gehigbie@gmail.com>
2018-03-06 16:29:02 +0000

    Reconfigure app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import VideoSearchApp from './VideoSearchApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<VideoSearchApp />, document.getElementById('react-container'));
 registerServiceWorker();
