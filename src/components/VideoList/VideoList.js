124d39630a5bcc3a3b42bfdfa0afd15e7856b894
George Higbie <gehigbie@gmail.com>
2018-03-09 16:01:26 +0000

    Pass video prop to vidoe list item

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -6,7 +6,7 @@ const VideoList = (props) => {
     return (
         <ul className="col-md-4 list group">
             {props.videos.map((video) => (
-                <VideoListItem />)
+                <VideoListItem video={video}/>)
             )}
         </ul>
     );
