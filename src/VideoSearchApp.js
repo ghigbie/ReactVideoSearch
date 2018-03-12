3d001d3f4cf3e9ea8547edb3dc64ca7eb2fff792
George Higbie <gehigbie@gmail.com>
2018-03-12 00:03:16 +0000

    Import video detail component into app

--- src/VideoSearchApp.js
+++ src/VideoSearchApp.js
@@ -4,6 +4,7 @@ import './VideoSearchApp.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import VideoList from './components/VideoList/VideoList';
+import VideoDetail from './components/VideoDetail/VideoDetail';
 import APIKey from './services/apiKey';
 
 class App extends Component {
