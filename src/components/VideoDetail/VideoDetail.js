db6e28df0ca57f1f5a2424be6a626228f04f99f6
George Higbie <gehigbie@gmail.com>
2018-03-12 00:01:37 +0000

    Add video id and url

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 const VideoDetail = ({video}) => {
-  return(
-        <div className="video-detail col-md-12">
-            <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item"></iframe>
+    const videoId = video.id.videoId;
+    const videoUrl = `https://www.youtube.com/embed/${videoId}`;
+    
+     return(
+            <div className="video-detail col-md-12">
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item"
+                            src={videoUrl}></iframe>
+                </div>
+                <div className="details">
+                    <div>{video.snippet.title}</div>
+                    <div>{video.snippet.description}</div>
+                </div>
             </div>
-            <div className="details">
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-      );  
+          );  
 };
