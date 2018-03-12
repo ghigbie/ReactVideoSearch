20c7548d16da014901d09161f32452739b0d6eaa
George Higbie <gehigbie@gmail.com>
2018-03-12 00:04:26 +0000

    Add video detail component

--- src/VideoSearchApp.js
+++ src/VideoSearchApp.js
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBar />
-        {console.log(this.state.videos)}
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
